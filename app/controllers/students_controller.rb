7acd7700ffaf266ee4b9972eb68b9f285d740dee
prodigysurge <boatinglife6@yahoo.com>
2021-03-12 12:38:29 -0500

    commit tree 0938c098995b88fc43bbfa1c43c11a32ae7e4197

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ActionController::Base
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+###
+  private
+   def student_params
+    params.require(:student).permit!
+  end
+  
+end
+
+
+
